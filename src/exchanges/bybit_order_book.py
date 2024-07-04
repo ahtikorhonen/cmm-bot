41d781b53205d59c0e2a506cc3ebb23498c8720c
ahtikorhonen <ahti.korhonen@aalto.fi>
2024-07-04 22:56:36 +0300

    removed bba subscription from htx data feed

--- src/exchanges/bybit_order_book.py
+++ src/exchanges/bybit_order_book.py
@@ -28,25 +28,4 @@ class BybitOrderBook(BaseOrderBook):
             self.asks = self.update(self.asks, asks)
             self.bids = self.update(self.bids, bids)
             self.sort()
-                
-    def process_bba(self, recv: Dict):
-        """
-        Handles incoming WebSocket messages to update the best bids and asks.
 
-        Parameters
-        :recv (Dict): the incoming message containing latest best bids and asks.
-        """
-        best_ask = recv["data"]["a"]
-        best_bid = recv["data"]["b"]
-        
-        if best_bid:
-            price, quantity = list(map(float, best_bid[0]))
-            if quantity > 0:
-                self.bba[0, 0] = price
-                self.bba[0, 1] = quantity
-
-        if best_ask:
-            price, quantity = list(map(float, best_ask[0]))
-            if quantity > 0:
-                self.bba[1, 0] = price
-                self.bba[1, 1] = quantity
