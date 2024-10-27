63fd5767d8d21a4e3060f6f3308dc55cc9ee9087
ahtikorhonen <ahti.korhonen@aalto.fi>
2024-10-27 13:37:39 +0200

    removed htx ws feed

--- src/data_feeds/data_feeds.py
+++ src/data_feeds/data_feeds.py
@@ -2,7 +2,6 @@ import asyncio
 
 from src.market_data import MarketData
 from src.data_feeds.bybit_data_feed import BybitDataFeed
-from src.data_feeds.htx_data_feed import HTXDataFeed
 from src.data_feeds.binance_data_feed import BinanceDataFeed
 
 class DataFeeds:
@@ -16,7 +15,6 @@ class DataFeeds:
         """
         tasks = [
             asyncio.create_task(BybitDataFeed(self.market_data.bybit_order_book).run()),
-            #asyncio.create_task(HTXDataFeed(self.market_data.htx_order_book).run()),
             asyncio.create_task(BinanceDataFeed(self.market_data.binance_order_book).run())
         ]
         
